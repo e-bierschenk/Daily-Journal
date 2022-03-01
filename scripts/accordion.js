e94c0f70bd93d34e2325d3c968d642512b67c961
e-bierschenk <bushanks@gmail.com>
2022-03-01 15:21:24 -0600

    cleaned up some code

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,12 +1,16 @@
+
+//TODO:  add closing of all other journal entries on click. element.classList.contains("journal__body--closed")
+
 export const buildAccordion = () => {
     const headerEl = document.querySelectorAll(".journal__header")
     
     for (const entry of headerEl) {
         entry.addEventListener("click", 
             function(){ 
-                // navigates to the parent container and selects the journal body of the associated journal entry
-                // then toggles the display:none with journal__body--closed
-            entry.parentElement.children[1].classList.toggle("journal__body--closed") 
-        })
+                    // navigates to the parent container and selects the journal body of the associated journal entry
+                    // then toggles the display:none with journal__body--closed
+                entry.parentElement.children[1].classList.toggle("journal__body--closed")
+            }
+        )
     }
 }
