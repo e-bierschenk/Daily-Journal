4115e32b02f79ff2228a242ffa2074ccb3afe541
e-bierschenk <bushanks@gmail.com>
2022-03-29 14:56:07 -0500

    added CRUD

--- scripts/data/dataHandlers.js
+++ scripts/data/dataHandlers.js
@@ -1,6 +1,6 @@
 let postCollection = []
 
-export const usePostCollection = () => {
+export const useJournals = () => {
     //Best practice: we don't want to alter the original state, so
     //make a copy of it and then return it
     //The spread operator makes this quick work
@@ -14,4 +14,41 @@ export const getAPIData = () => {
         postCollection = parsedResponse
         return parsedResponse
     })
+}
+
+export const createPost = (postObj) => {
+    return fetch("http://localhost:8088/entries", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(postObj)
+    })
+    .then(response => response.json())
+}
+
+export const deletePost = (postId) => {
+    return fetch(`http://localhost:8088/entries/${postId}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    .then(response => response.json())
+}
+
+export const getSinglePost = (postId) => {
+    return fetch(`http://localhost:8088/entries/${postId}`)
+    .then(response => response.json())
+}
+
+export const updatePost = (postObj) => {
+    return fetch(`http://localhost:8088/entries/${postObj.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(postObj)
+    })
+    .then(response => response.json())
 }
--- scripts/journalEntryList.js
+++ scripts/journalEntryList.js
@@ -4,13 +4,15 @@
  *    there are items in the collection exposed by the
  *    data module component
  */
-import { usePostCollection } from "./data/dataHandlers.js"
-import { journalEntryComponent } from "./journalEntry.js"
+import { useJournals } from "./data/dataHandlers.js"
+import { journalEntryComponent, journalForm } from "./journalEntry.js"
 
-// DOM reference to where all entries will be rendered
-const entryLog = document.querySelector("#entryLog")
 
 export const renderJournal = (journalList) => {
+    // DOM reference to where all entries will be rendered
+    const entryLog = document.querySelector("#entryLog")
+
+    entryLog.innerHTML=''
     // Use the journal entry data from the data module component
     for (const entry of journalList) {
         /*
@@ -18,5 +20,20 @@ export const renderJournal = (journalList) => {
             HTML representation of a single entry
         */
         entryLog.innerHTML += journalEntryComponent(entry)
+    }
+}
+
+export const renderForm = (postObj = null) => {
+    const formEl = document.querySelector("#journalForm")
+    formEl.innerHTML = journalForm(postObj)
+
+    if(postObj){
+        const conceptEl = document.querySelector("#journalConcepts")
+        const entryEl = document.querySelector("#journalText")
+        const selectEl = document.querySelector("#journalMood")
+
+        conceptEl.value = postObj.concept
+        entryEl.value = postObj.entry
+        selectEl.value = postObj.mood.toLowerCase()
     }
 }
